5ed5bd1537ec03f0e6cd6e95c538d3ab3668a608
jjijju <cheytac406@gmail.com>
2021-07-29 16:43:53 +0900

    Feat: state로 값 변경

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	let [title, setTitle] = useState(['mock title1', 'mock title2', 'mock title3']);
+	let [like, setLike] = useState(0);
+
+	const changeTitle = () => {
+		const newArray = [...title];
+		newArray[0] = 'mock title change';
+		setTitle(newArray);
+	}
+
+	return (
+		<div className="App">
+			<div className="black-nav">
+				<div>개발 Blog</div>
+			</div>
+			<h4>Blog Title</h4>
+			<button onClick={changeTitle}>수정</button>
+			<div className="list">
+				<h3> {title[0]} <span onClick={ () => { setLike(like + 1); } }>👍</span> {like} </h3>
+				<p className="list-descript">2020/07/29</p>
+				<hr />
+			</div>
+			<div className="list">
+				<h3> {title[1]} </h3>
+				<p className="list-descript">2020/07/29</p>
+				<hr />
+			</div>
+			<div className="list">
+				<h3> {title[2]} </h3>
+				<p className="list-descript">2020/07/29</p>
+				<hr />
+			</div>
+		</div>
+	);
 }
 
 export default App;
